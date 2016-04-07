b34b20ae7feada242105dbe9da055fb8406a74b3
Jason Tu <jasontu4@gmail.com>
2016-04-07 17:50:50 -0400

    update config files

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,11 +1,12 @@
-/*jshint node:true*/
+
+/* jshint node:true */
 /* global require, module */
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var EmberApp = require('ember-cli/lib/broccoli/ember-app')
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
-  });
+  })
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
@@ -20,5 +21,6 @@ module.exports = function(defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  return app.toTree();
-};
+  return app.toTree()
+}
+
